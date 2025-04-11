532ad550545f03a3044906f643bce15906975dcb
vwj1776 <vwj1776@gmail.com>
2025-04-10 20:49:24 -0600

    changed login to start to work

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src', // Change this to the folder containing index.html
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // match your Express server port
+    }
+  },
 });
